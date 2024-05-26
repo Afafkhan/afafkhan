e238ea7b73ef24490075283af7c2cc6967f27308
Afafkhan <khan.afaf13@gmail.com>
2024-05-26 20:14:10 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let Num1 = 0
+let Num2 = 0
+let Result = 0
+input.onButtonPressed(Button.A, function () {
+    Num1 = 70
+    Num2 = 50
+    if (Num1 < Num2) {
+        Result = Num2 - Num1
+        basic.showNumber(Result)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
